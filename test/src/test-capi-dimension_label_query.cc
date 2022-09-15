0535d9ebeddb286baa0130828faa03f5fcfaeec8
Julia Dark <julia@tiledb.com>
2022-09-15 16:58:50 -0400

    Adds test for dense array with dimension labels

--- test/src/test-capi-dimension_label_query.cc
+++ test/src/test-capi-dimension_label_query.cc
@@ -172,6 +172,122 @@ class SparseArrayExample1 : public DimensionLabelFixture {
   double label_domain_[2];
 };
 
+/**
+ * Create a small dense array with a dimension label.
+ *
+ */
+template <tiledb_label_order_t label_order>
+class DenseArrayExample1 : public DimensionLabelFixture {
+ public:
+  DenseArrayExample1()
+      : index_domain_{0, 3}
+      , label_domain_{-1, 1} {
+    // Create an array schema
+    uint64_t x_tile_extent{4};
+    auto array_schema = create_array_schema(
+        ctx,
+        TILEDB_DENSE,
+        {"x"},
+        {TILEDB_UINT64},
+        {&index_domain_[0]},
+        {&x_tile_extent},
+        {"a"},
+        {TILEDB_FLOAT64},
+        {1},
+        {tiledb::test::Compressor(TILEDB_FILTER_NONE, -1)},
+        TILEDB_ROW_MAJOR,
+        TILEDB_ROW_MAJOR,
+        4096,
+        false);
+
+    // Add dimension label.
+    double label_tile_extent = 2.0;
+    add_dimension_label(
+        ctx,
+        array_schema,
+        "x",
+        0,
+        label_order,
+        TILEDB_FLOAT64,
+        &label_domain_[0],
+        &label_tile_extent,
+        &x_tile_extent);
+    // Create array
+    array_name = create_temporary_array("array_with_label_1", array_schema);
+    tiledb_array_schema_free(&array_schema);
+  }
+
+  void write_array_with_label(
+      std::vector<double>& input_attr_data,
+      std::vector<double>& input_label_data) {
+    // Open array for writing.
+    tiledb_array_t* array;
+    require_tiledb_ok(tiledb_array_alloc(ctx, array_name.c_str(), &array));
+    require_tiledb_ok(tiledb_array_open(ctx, array, TILEDB_WRITE));
+
+    // Define sizes for setting buffers.
+    uint64_t attr_data_size{input_attr_data.size() * sizeof(double)};
+    uint64_t label_data_size{input_label_data.size() * sizeof(double)};
+
+    // Create subarray.
+    tiledb_subarray_t* subarray;
+    require_tiledb_ok(tiledb_subarray_alloc(ctx, array, &subarray));
+    require_tiledb_ok(tiledb_subarray_add_range(
+        ctx, subarray, 0, &index_domain_[0], &index_domain_[1], nullptr));
+
+    // Create write query.
+    tiledb_query_t* query;
+    require_tiledb_ok(tiledb_query_alloc(ctx, array, TILEDB_WRITE, &query));
+    require_tiledb_ok(tiledb_query_set_layout(ctx, query, TILEDB_ROW_MAJOR));
+    require_tiledb_ok(tiledb_query_set_subarray_t(ctx, query, subarray));
+    if (attr_data_size != 0) {
+      require_tiledb_ok(tiledb_query_set_data_buffer(
+          ctx, query, "a", input_attr_data.data(), &attr_data_size));
+    }
+    if (label_data_size != 0) {
+      require_tiledb_ok(tiledb_query_set_label_data_buffer(
+          ctx, query, "x", input_label_data.data(), &label_data_size));
+    }
+
+    // Submit write query.
+    require_tiledb_ok(tiledb_query_submit(ctx, query));
+    tiledb_query_status_t query_status;
+    require_tiledb_ok(tiledb_query_get_status(ctx, query, &query_status));
+    REQUIRE(query_status == TILEDB_COMPLETED);
+
+    // Clean-up.
+    tiledb_query_free(&query);
+    tiledb_array_free(&array);
+  }
+
+  std::vector<double> read_indexed_array() {
+    return DimensionLabelFixture::read_indexed_array<double>(
+        URI(array_name).join_path("__labels/l0"),
+        4,
+        &index_domain_[0],
+        &index_domain_[1]);
+  }
+
+  std::tuple<std::vector<uint64_t>, std::vector<double>> read_labelled_array() {
+    return DimensionLabelFixture::read_labelled_array<uint64_t, double>(
+        URI(array_name).join_path("__labels/l0"),
+        4,
+        &label_domain_[0],
+        &label_domain_[1]);
+  }
+
+ protected:
+  /** Name of the example array. */
+  std::string array_name;
+
+ private:
+  /** Valid range for the index. */
+  uint64_t index_domain_[2];
+
+  /** Valid range for the label. */
+  double label_domain_[2];
+};
+
 TEST_CASE_METHOD(
     SparseArrayExample1<TILEDB_INCREASING_LABELS>,
     "Write increasing dimension label and array for sparse 1D array",
@@ -225,6 +341,36 @@ TEST_CASE_METHOD(
 
   // Read back and check labelled array.
   {
+    auto [index_data, label_data] = read_labelled_array();
+    // Check results.
+    for (uint64_t ii{0}; ii < 4; ++ii) {
+      CHECK(label_data[ii] == input_label_data[ii]);
+    }
+    for (uint64_t ii{0}; ii < 4; ++ii) {
+      CHECK(index_data[ii] == input_index_data[ii]);
+    }
+  }
+}
+
+TEST_CASE_METHOD(
+    DenseArrayExample1<TILEDB_INCREASING_LABELS>,
+    "Write increasing dimension label and array for dense 1D array",
+    "[capi][query][DimensionLabel]") {
+  std::vector<double> input_label_data{-1.0, 0.0, 0.5, 1.0};
+  std::vector<double> input_attr_data{0.5, 1.0, 1.5, 2.0};
+  write_array_with_label(input_attr_data, input_label_data);
+  // Read back and check indexed array.
+  {
+    auto label_data = read_indexed_array();
+    // Check results.
+    for (uint64_t ii{0}; ii < 4; ++ii) {
+      CHECK(label_data[ii] == input_label_data[ii]);
+    }
+  }
+
+  // Read back and check labelled array.
+  {
+    std::vector<uint64_t> input_index_data{0, 1, 2, 3};
     auto [index_data, label_data] = read_labelled_array();
     // Check results.
     for (uint64_t ii{0}; ii < 4; ++ii) {
