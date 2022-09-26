7f1c2953e9c1303680d7f0f971adb4735a4c4101
Ypatia Tsavliri <ypatia@tiledb.com>
2022-09-26 17:05:49 +0300

    UTs and fixes part2

--- tiledb/sm/fragment/fragment_metadata.h
+++ tiledb/sm/fragment/fragment_metadata.h
@@ -1021,6 +1021,11 @@ class FragmentMetadata {
     return has_delete_meta_;
   }
 
+  /** has_consolidated_footer accessor */
+  bool& has_consolidated_footer() {
+    return has_consolidated_footer_;
+  }
+
   /** sparse_tile_num accessor */
   uint64_t& sparse_tile_num() {
     return sparse_tile_num_;
